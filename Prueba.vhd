0a32fe04d40ea941f815e032eb1cd77f0b6af661
Perea-Daniel <perea.ncer@gmail.com>
2021-07-28 19:22:24 -0500

    mi primer commit

---
+++ Prueba.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+--use IEEE.numeric_std.all;
+
+entity Prueba is
+    --generic();
+    port(
+        A : in std_logic_vector(2 downto 0);
+        B : out std_logic_vector(2 downto 0);
+        );
+end entity;
+
+architecture RTL of Prueba is
+    
+begin
+    
+end RTL;
